884ac1aa3b0514b041556d261a9828aaeae9e46b
Jordan <jadamek2@kent.edu>
2018-03-31 19:28:53 -0400

    - Added Isometric Tile

--- Tactics.Net/Isogeometry/IsometricObject.cs
+++ Tactics.Net/Isogeometry/IsometricObject.cs
@@ -1,20 +1,23 @@
-﻿using SFML.System;
-using System;
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using Tactics.Net.Extensions;
 
 namespace Tactics.Net.Isogeometry
 {
     //========================================================================================================================
-    // ** Isometric Object
+    // ** Isometric Object (Abstract)
     //========================================================================================================================
     // Represents an object which is drawn using an isometric angle.
     //========================================================================================================================
-    public class IsometricObject
+    public abstract class IsometricObject : Disposable, Drawable
     {
         //--------------------------------------------------------------------------------------------------------------------
         // - Get Height
         //--------------------------------------------------------------------------------------------------------------------
         // * offset : (x,y) position relative to the center of the x-y axis of object
         //--------------------------------------------------------------------------------------------------------------------
+        public float Height() { return Height(new Vector2f(0, 0)); }
         public virtual float Height(Vector2f offset)
         {
             return 1.0f;
@@ -27,6 +30,11 @@ namespace Tactics.Net.Isogeometry
         {
             PositionChanged?.Invoke(this, new PositionChangedEventArgs() { OldPosition = oldPosition, NewPosition = newPosition, });
         }
+
+        //--------------------------------------------------------------------------------------------------------------------
+        // - Draw (Abstract, Implementation)
+        //--------------------------------------------------------------------------------------------------------------------
+        public abstract void Draw(RenderTarget target, RenderStates states);
 
         //--------------------------------------------------------------------------------------------------------------------
         // - Position (Property)
---
+++ Tactics.Net/Map/Tile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using Tactics.Net.Isogeometry;
+
+namespace Tactics.Net.Map
+{
+    //========================================================================================================================
+    // ** Isometric Tile
+    //========================================================================================================================
+    // Represents a single basic isometric map tile with a float height and single occupant (object directly above this one)
+    //========================================================================================================================
+    public class Tile : IsometricObject
+    {
+        public Tile(Sprite sprite = null, float height = 1.0f)
+        {
+            Sprite = sprite;
+
+            // If the Z-coordinate is changed, appropriately adjust the Z-coordinate of any occupying objects 
+            PositionChanged += (s, e) =>
+            {
+                if(e.NewPosition.Z != e.OldPosition.Z)
+                {
+                    Occupant.Position = new Vector3f(Occupant.Position.X, Occupant.Position.Y, Occupant.Position.Z + e.NewPosition.Z - e.OldPosition.Z);
+                }
+            };
+        }
+
+        //--------------------------------------------------------------------------------------------------------------------
+        // - Set Height
+        //--------------------------------------------------------------------------------------------------------------------
+        // Set the flat height of this tile and appropriately adjust the Z-coordinate of any occupying objects
+        //--------------------------------------------------------------------------------------------------------------------
+        public virtual void SetHeight(float height)
+        {
+            if (height_ != height)
+            {
+                if(Occupant != null)
+                {
+                    Occupant.Position = new Vector3f(Occupant.Position.X, Occupant.Position.Y, Occupant.Position.Z + height - height_);
+                }
+                height_ = height;
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------------
+        // - Draw Tile (Override)
+        //--------------------------------------------------------------------------------------------------------------------
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            Sprite?.Draw(target, states);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------------
+        // - Dispose (Extension)
+        //--------------------------------------------------------------------------------------------------------------------
+        public override void Dispose()
+        {
+            Sprite?.Dispose();
+            base.Dispose();
+        }
+
+        // Members
+        public IsometricObject Occupant { get; set; }
+        protected Sprite Sprite { get; set; }
+        protected float height_;
+    }
+}

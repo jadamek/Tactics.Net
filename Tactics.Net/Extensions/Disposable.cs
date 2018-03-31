0c026ef3a293e9a71385852f47bddbc2d2c271a3
Jordan <jadamek2@kent.edu>
2018-03-31 16:37:44 -0400

    Added Disposable extension which implements a Disposed event on disposal.

---
+++ Tactics.Net/Extensions/Disposable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tactics.Net.Extensions
+{
+    //====================================================================================================
+    // ** IDisposable Extension
+    //====================================================================================================
+    public class Disposable : IDisposable
+    {
+        //------------------------------------------------------------------------------------------------
+        // - Dispose And Notify
+        //------------------------------------------------------------------------------------------------
+        // Extension which connects Dispose to the 'Disposed' event. Meant to be extended for actual
+        // cleanup logic
+        //------------------------------------------------------------------------------------------------
+        public virtual void Dispose()
+        {
+            OnDisposal();
+        }
+
+        //------------------------------------------------------------------------------------------------
+        // - Fire Disposed Event
+        //------------------------------------------------------------------------------------------------
+        protected virtual void OnDisposal()
+        {
+            Disposed?.Invoke(this, new EventArgs());
+        }
+
+        // Members - Events
+        public event EventHandler<EventArgs> Disposed;
+    }
+}

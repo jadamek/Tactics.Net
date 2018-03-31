97e6006ee5e42472b6d7970a53227aa092821ee1
Jordan <jadamek2@kent.edu>
2018-03-31 16:14:49 -0400

    Base program with window creation and basic display loop.

---
+++ Tactics.Net/Tactics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tactics.Net
+{
+    class Tactics
+    {
+        static void Main()
+        {
+            SFML.Graphics.RenderWindow window = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(640, 480), "Tactics!");
+            window.Closed += (s, e) => { window.Close(); };
+
+            while(window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear();
+                window.Display();
+            }
+        }
+    }
+}
